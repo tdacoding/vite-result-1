6380838b8097d480b336f35dc41f9d470e75b5c6
Dmitry Timushev <timushev@gmail.com>
2025-02-12 22:09:21 +0300

    second task

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,34 @@
 import logo from './assets/logo.svg';
 import './App.css';
+import { createElement } from 'react';
 
 function App() {
-	return (
-		// декларативный стиль
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>
-					{/* императивный стиль*/}
-					{new Date().getFullYear()}
-				</p>
-			</header>
-		</div>
+	return createElement(
+		'div',
+		{ className: 'App' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+			createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }, null),
+			createElement(
+				'p',
+				null,
+				'Edit ',
+				createElement('code', null, 'src/App.js'),
+				' and save to reload.',
+			),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			createElement('p', null, new Date().getFullYear()),
+		),
 	);
 }
 
